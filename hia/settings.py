7b0cea7811fc392be75b6cd360def7d87644065b
theo <tkleinen@gmail.com>
2019-11-23 18:31:04 +0100

    redis caching added

--- hia/settings.py
+++ hia/settings.py
@@ -132,10 +132,21 @@ GRAPPELLI_ADMIN_TITLE='Beheer van grondwatermeetnet Hendrik-Ido-Ambacht'
 ACCOUNT_ACTIVATION_DAYS = 7
 LOGIN_REDIRECT_URL = '/data/'
 
+# Caching
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+# Logging
 LOGGING_ROOT = os.path.join(BASE_DIR, 'logs')
 LOGGING_URL = '/logs/'
 
-# Logging
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
